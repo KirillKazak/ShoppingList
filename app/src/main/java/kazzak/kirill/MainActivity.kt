af9f94f8b1f18a1efb81fe826e177fbb36428ba6
Kirill Kazak <kazzak.kirill1995@gmail.com>
2022-08-14 16:47:07 +0300

    Initial commit

---
+++ app/src/main/java/kazzak/kirill/MainActivity.kt
@@ -0,0 +1,11 @@
+package kazzak.kirill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
